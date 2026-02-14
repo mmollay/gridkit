04ed71cd451e48cdd7e7c313677b13bfe96a4e07
mmollay <martin@ssi.at>
2026-02-14 09:01:49 +0100

    fix: fehlende CSS-Farben für StatCards + Material Icons in Demo

--- demo/index.php
+++ demo/index.php
@@ -14,6 +14,7 @@ use GridKit\YearFilter;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GridKit Demo</title>
     <link rel="stylesheet" href="../css/gridkit.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <style>
         body {
             margin: 0;
